3bb7f12c18138e543a67c09f65b3d72a4e6161c4
Mustafa <mbin828@gmail.com>
2022-03-09 19:48:17 -0500

    mvp down working on dynamic coloring of sliders

--- script.js
+++ script.js
@@ -1,6 +1,36 @@
 // api:https://www.boredapi.com/api
+
 let getRandom = async () => {
   let response = await axios.get("https://www.boredapi.com/api/activity");
   let data = response.data;
-  console.log(data);
+  console.log("key:  ", data["key"]);
+  let activity = data["activity"];
+  let type = data["type"];
+  let accessibility = data["accessibility"] * 100;
+  if (accessibility != 100) {
+    accessibility = 100 - accessibility;
+  }
+  let price = data["price"] * 100;
+  document.querySelector("#activity").textContent = activity;
+  document.querySelector("#type").textContent = type;
+  const priceBar = document.querySelector(".price-meter-bar");
+  priceBar.style.width = price + "%";
+  const accessBar = document.querySelector(".access-meter-bar");
+  accessBar.style.width = accessibility + "%";
+  if (accessibility < 40) {
+    accessBar.style.backgroundColor = "#ff0008";
+  } else {
+    accessBar.style.backgroundColor = "#10f700";
+  }
+  if (price > 50) {
+    if (price < 7) {
+      priceBar.style.backgroundColor = "#FFFFFF";
+    } else {
+      priceBar.style.backgroundColor = "#ff0008";
+    }
+  } else {
+    priceBar.style.backgroundColor = "#10f700";
+  }
 };
+
+document.querySelector("#generate").addEventListener("click", getRandom);
